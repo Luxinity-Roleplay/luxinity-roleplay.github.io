a10cbf3de059b8107e7f98160d5f1eff1618b874
Dennys Matthew <27568445+mclemie@users.noreply.github.com>
2022-05-09 01:26:48 +0700

    better index layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,8 +29,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`Homepage`}
+      description="Luxinity Roleplay is a SA:MP Server Based in 2022 Los Santos & Red Country.">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
