e16c9933af77129badddb5babf4f568ccbda2a18
Dennys Matthew <27568445+mclemie@users.noreply.github.com>
2022-05-12 13:14:27 +0700

    better meta tags

--- theme.config.js
+++ theme.config.js
@@ -12,19 +12,16 @@ export default {
   ),
   head: (
     <>
-      <meta name="msapplication-TileColor" content="#ffffff" />
-      <meta name="theme-color" content="#ffffff" />
+      <meta name="msapplication-TileColor" content="#fb4127" />
+      <meta name="theme-color" content="#fb4127" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Offcial Luxinity Roleplay Website and Wiki" />
-      <meta name="og:description" content="Offcial Luxinity Roleplay Website and Wiki" />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:image" content="https://media.discordapp.net/attachments/846736449705082920/967445380751056896/LX.png?width=468&height=468" />
-      <meta name="twitter:site:domain" content="luxinity-roleplay.github.io" />
-      <meta name="twitter:url" content="https://luxinity-roleplay.github.io" />
       <meta name="og:title" content="Luxinity" />
-      <meta name="og:image" content="https://media.discordapp.net/attachments/846736449705082920/967445380751056896/LX.png?width=468&height=468" />
-      <meta name="apple-mobile-web-app-title" content="Luxinity" />
+      <meta name="author" content="mclemie" />
+      <meta name="description" content="Official Luxinity Roleplay Wiki" />
+      <meta name="og:description" content="Official Luxinity Roleplay Wiki" />
+      <meta property="og:image" content="https://media.discordapp.net/attachments/846736449705082920/967445380751056896/LX.png?width=468&height=468" />
+      <meta name="keywords" content="samp, samp indo, samp roleplay, gta, san andreas multiplayer" />
       <link
         rel="icon"
         type="image/png"
